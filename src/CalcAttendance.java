55c1f1ec56a684bb7a27860836572c7b474ab4ba
Sally <sally@192.168.1.12>
2017-10-21 22:43:00 -0700

    Rough draft

--- src/CalcAttendance.java
+++ src/CalcAttendance.java
@@ -1,11 +1,37 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+import javax.imageio.ImageIO;
+
 public class CalcAttendance {
+	private BufferedImage image = null;
+	private int studentCount = 0;
 	
-	public CalcAttendance() {
-		
+	public CalcAttendance() throws IOException {
+		try {
+		    image = ImageIO.read(new File("c:\\"));
+		} catch (IOException e) {
+			throw new IOException("Image not found.");
+		}
 	}
 	
-	public void takeAttendance() {
-		
+	// Parse through photos of students, and determine whether student is present or not.
+	public void takeAttendance(BufferedImage image, Map<String, BufferedImage> studentsHere) {
+		Map<String, Boolean> isPresent = new HashMap<String, Boolean>();
+		for (String student : studentsHere.keySet()) {
+			if (/* if student is present */) {
+				isPresent.put(student, true);
+				studentCount++;
+			} else {
+				isPresent.put(student, false);
+			}
+		}
 	}
 	
+	// Get count of students who are currently here.
+	public int getStudentCount() {
+		return studentCount;
+	}
 }
